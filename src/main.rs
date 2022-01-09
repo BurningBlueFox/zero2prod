f2ac0511033f9713cfc0b1bdb4d177b7ee4c3812
Thiago Leao <thiagogorgulho@outlook.com>
2022-01-09 14:05:49 -0300

    Bind app to a random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("App bound to address: http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
