0d0ac874ca1dc7b617c8a6991c249b7fae91b73c
Thiago Leão <thiagogorgulho@outlook.com>
2022-01-16 16:51:44 -0300

    Fix typo on sh script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
-DB_USER=${POSTGRES_PASSWORD:=password}
+DB_PASSWORD=${POSTGRES_PASSWORD:=password}
 # Check if a custom database name has been set, otherwise default to 'newsletter'
-DB_USER=${POSTGRES_DB:=newsletter}
+DB_NAME=${POSTGRES_DB:=newsletter}
 # Check if a custom port has been set, otherwise default to '5432'
-DB_USER=${POSTGRES_PORT:=5432}
+DB_PORT=${POSTGRES_PORT:=5432}
 
 # Launch postgres using docker
 docker run \
@@ -17,6 +17,5 @@ docker run \
 -e POSTGRES_PASSWORD=${DB_PASSWORD} \
 -e POSTGRES_DB=${DB_NAME} \
 -p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
+-d postgres
 # ^ Increased maximum number of connections for testing purposes
